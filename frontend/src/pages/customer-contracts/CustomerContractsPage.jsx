3857fd742209fbad239d328016780211431db64b
VDimov01 <vdimov54@gmail.com>
2025-10-10 14:36:52 +0300

    Update CustomerContractsPage.jsx

--- frontend/src/pages/customer-contracts/CustomerContractsPage.jsx
+++ frontend/src/pages/customer-contracts/CustomerContractsPage.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import "./CustomerContractsPage.css"; // <-- import the CSS file above
 
 function buildUrl(base, path, params = {}) {
   const root = (base || "").replace(/\/+$/, "");
